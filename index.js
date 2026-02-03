ca64d053235fe4c0755d2a99b52bf7bb2112a3d2
mdriyazakondo <mdriyazakondo265@gmail.com>
2026-02-03 17:58:23 +0600

    fronend url

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, 
+    origin: ["http://localhost:5173", "https://biz-cart-client.vercel.app/"],
+    credentials: true,
   }),
 );
 app.use(express.json());
