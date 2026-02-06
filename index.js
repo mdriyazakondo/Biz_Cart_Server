c2db26df62db0c7c398c004c8bdcadc8d1cda11d
mdriyazakondo <mdriyazakondo265@gmail.com>
2026-02-07 00:10:28 +0600

    url change

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://biz-cart-client.vercel.app/"],
     credentials: true,
   }),
 );
